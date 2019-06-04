f6de477b2180a00c0ea8bff1eda867c8f973ddf4
lizKimita <kimita.wanjiku@gmail.com>
2019-06-04 14:45:40 +0300

    running migrations and adding modeltables to admin file

--- wedlist_forums/admin.py
+++ wedlist_forums/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Status,Posts,Profile
 
 # Register your models here.
+admin.site.register(Status)
+admin.site.register(Posts)
+admin.site.register(Profile)
